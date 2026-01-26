dffd79090a894efd2d310cc0d511e5822ae82ea4
Suryabhat48 <suryabhat2032@gmail.com>
2026-01-26 20:46:28 +0530

    Adding the variable features

--- var.cpp
+++ var.cpp
@@ -1,15 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"Hello this is my first cpp program and I am Surya!"<<endl;
+    // cout<<"Hello this is my first cpp program and I am Surya!"<<endl;
     int age=25;
     cout<<sizeof(age)<<endl;
     char grade='A';
     float pi=3.14f;
+    age=grade; // implicit typecasting
+    cout<<age<<endl; 
  // Until and unless we write the f suffix, the compiler treats decimal values as double by default.eturn 0;
     bool isAdult=true;
     double largeDecimal=3.141592653589793;
     age=grade; // implicit typecasting
-    cout<<age<<endl;     
+        
     return 0;
 }
