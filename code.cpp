145807d74ed4fa78488a8ace9f69cb81c64f54ff
Suryabhat48 <suryabhat2032@gmail.com>
2026-01-28 16:39:32 +0530

    Added the calculated stuffs learning new things

---
+++ code.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number "<<endl;
+    cin>>n;
+    if(n>=0){
+        cout<<"The number is positive"<<endl;
+    }
+    
+    // 	int age;
+// 	cout<<"Enter your age"<<endl;
+// 	cin>>age;
+// 	if(age>=18){
+// 	    cout<<"Congrats you are eligible for voting"<<endl;
+// 	}
+// 	else{
+// 	    cout<<"Sorry you can't vote"<<endl;
+	    
+// 	}
+// 	int n;
+// 	cout<<"Enter a number"<<endl;
+// 	if(n%2==0){
+// 	    cout<<"Even number"<<endl;
+// 	}
+// 	else{
+// 	    cout<<"Odd number"<<endl;
+// 	}\
+// Prgram to calculate sum of numbers from 1 to n 
+int n,sum=0;
+cout<<"Enter the number "<<endl;
+cin>>n;
+// // for(int i=1;i<=n;i++){
+// //     sum=sum+i;
+// // }
+// // cout<<"The sum of n numbers is "<<sum<<endl;
+// int i=1;
+// while(i<=n){
+//     sum+=i;
+//     i++;
+// }
+// cout<<"The sum of the numbers one to n is"<<sum<<endl;
+// // 	Program to calculate sum of even  numbers
+// for(int i=1;i<=n;i++){
+//     if(i%2==0){
+//         sum+=i;
+//     }
+// }
+// cout<<"Sum of even numbers from one to n is "<<sum<<endl;
+// int oddSum=0;
+// int i=1;
+// while(i<=n){
+//     if(i%2!=0){
+//         oddSum+=i;
+//     }
+//     i++;
+// }
+// cout<<"ODD SUM IS "<<oddSum<<endl;
+// Program to check whether the number is prime or not using the unique factors approach/
+// int isPrime=true;
+// for(int i=2;i*i<=n;i++){
+//     if(n%i==0){
+//         isPrime=false;
+//         break;
+//     }
+// }
+// if(isPrime){
+//     cout<<"The number that you have entered is prime number"<<endl;
+    
+// }
+// else{
+//     cout<<"The number that you have entered is composite number"<<endl;
+// }
+// Program to calculate the sum of all numbers which are divisible by 3
+// int divSum=0;
+// for(int i=1;i<=n;i++){
+//     if(i%3==0){
+//         divSum+=i;
+//     }
+// }
+// cout<<"Sum of the numbers which are divisible by n is "<<divSum<<endl;
+// Program to calculate the factorial of the numbers
+int fact=1;
+for(int i=1;i<=n;i++){
+    fact*=i;
+}
+cout<<"Factorial of the given number is "<<fact<<endl;
+    return 0;
+    
+}
